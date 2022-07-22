5c81956565e3973ba4ae7aab19aa3018ae18bd6e
iamamitkumarsaini <iamaksaini68@gmail.com>
2022-07-23 03:34:12 +0530

    signin Page & signup Page

---
+++ signupPage.js
@@ -0,0 +1,50 @@
+document.getElementById("navbarImage").addEventListener("click",navbarImageFunc);
+
+function navbarImageFunc(){
+    window.location.href = "index.html"
+};
+
+document.getElementById("navbarSignupBtn").addEventListener("click",navbarSignupFunc);
+
+function navbarSignupFunc (){
+    window.location.href = "signupPage.html"
+};
+
+
+document.getElementById("navbarSigninBtn").addEventListener("click",navbarSigninFunc);
+
+function navbarSigninFunc(){
+    window.location.href = "signinPage.html"
+};
+
+
+
+
+document.getElementById("continueBtn").addEventListener("click",signUpFunction);
+
+
+let signupArr = JSON.parse(localStorage.getItem("signupData")) || [];
+
+function signUpFunction(){
+    let signupObj = {
+        mail:document.getElementById("signinmail").value,
+        password:document.getElementById("signinpassword").value
+    };
+
+
+  if (signupObj["mail"] == ""  || signupObj["password"] == "" ){
+    document.getElementById("continueBtn").disabled = true;
+    alert("Fill all the input fields")
+    return;
+  }  
+
+  else{
+    signupArr.push(signupObj);
+    document.getElementById("continueBtn").style.backgroundColor = "#1a73e8"
+    window.location.href = "signinPage.html"
+    document.getElementById("signinmail").value = "";
+    document.getElementById("signinpassword").value = "";
+  }
+
+  localStorage.setItem("signupData", JSON.stringify(signupArr))
+}
