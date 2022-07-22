9c56af04b2c08429a80f0bbaaf5563d631ad5770
iamamitkumarsaini <iamaksaini68@gmail.com>
2022-07-23 03:23:13 +0530

    homepage

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("navbarSignupBtn").addEventListener("click",navbarSignupFunc);
+
+function navbarSignupFunc (){
+    window.location.href = "signupPage.html"
+};
+
+
+document.getElementById("navbarSigninBtn").addEventListener("click",navbarSigninFunc);
+
+function navbarSigninFunc(){
+    window.location.href = "signinPage.html"
+};
