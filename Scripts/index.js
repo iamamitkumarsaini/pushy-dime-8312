ebe39fa6a6b0bea846b4bec36613793b469e3bf8
iamamitkumarsaini <iamaksaini68@gmail.com>
2023-02-12 17:22:51 +0530

    Unit-2

---
+++ Scripts/index.js
@@ -0,0 +1,18 @@
+document.getElementById("navbarSignupBtn").addEventListener("click",navbarSignupFunc);
+
+function navbarSignupFunc (){
+    window.location.href = "signupPage.html"
+};
+
+
+document.getElementById("navbarSigninBtn").addEventListener("click",navbarSigninFunc);
+
+function navbarSigninFunc(){
+    window.location.href = "signinPage.html"
+};
+
+document.querySelector("#findCourse").addEventListener("click",btnFunc)
+
+function btnFunc (){
+    window.location.href = "coursePage.html"
+};
