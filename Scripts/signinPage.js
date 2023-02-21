5533db33e33dbbec19685befebc8549b687398af
Amit Kumar Saini <107475338+iamamitkumarsaini@users.noreply.github.com>
2023-02-21 23:50:46 +0530

    Add files via upload

--- Scripts/signinPage.js
+++ Scripts/signinPage.js
@@ -1,35 +1,57 @@
-document.getElementById("navbarImage").addEventListener("click",navbarImageFunc);
-
-function navbarImageFunc(){
-    window.location.href = "index.html"
-};
-
-document.getElementById("navbarSignupBtn").addEventListener("click",navbarSignupFunc);
-
-function navbarSignupFunc (){
-    window.location.href = "signupPage.html"
-};
-
-
-document.getElementById("navbarSigninBtn").addEventListener("click",navbarSigninFunc);
-
-function navbarSigninFunc(){
-    window.location.href = "signinPage.html"
-};
-
-
-
-document.getElementById("signinBtn").addEventListener("click",signinFunc)
+let signinBtn = document.getElementById("signinBtn");
 
 let signinArr = JSON.parse(localStorage.getItem("signupData")) || [];
+let sMail = document.getElementById("signinmail");
+let sPassword = document.getElementById("signinpassword");
+
+let eyeicon1 = document.getElementById("eyeicon1");
+let eyeicon2 = document.getElementById("eyeicon2");
+
+eyeicon1.addEventListener("click",showPasswordFunc);
+eyeicon2.addEventListener("click",hidePasswordFunc);
+
+function showPasswordFunc(){
+    eyeicon1.style.display = "none";
+    eyeicon2.style.display = "block"
+
+    sPassword.type = "text";
+}
+
+function hidePasswordFunc(){
+    eyeicon1.style.display = "flex";
+    eyeicon2.style.display = "none"
+
+    sPassword.type = "password";
+}
+
+
+
+
+sMail.addEventListener("input",checkForText)
+sPassword.addEventListener("input",checkForText)
+
+function checkForText(){
+    if(sMail.value !=="" && sPassword.value !==""){
+        signinBtn.disabled = false
+        signinBtn.style.backgroundColor = "#1a73e8"
+        signinBtn.style.color = "#fff"
+    }
+    else{
+        signinBtn.disabled = true;
+        signinBtn.style.backgroundColor = "rgba(192,192,192,0.7)"
+        signinBtn.style.color = "rgba(150,150,150,1)"
+    }
+}
+
+
+signinBtn.addEventListener("click",signinFunc);
 
 function signinFunc(){
     let signinObj = {
-        sMail:document.getElementById("signinmail").value,
-        sPassword:document.getElementById("signinpassword").value
+        sMail:sMail.value,
+        sPassword:sPassword.value
     }
-
-    signinCheckFunc(signinObj)
+        signinCheckFunc(signinObj)
 };
 
 
@@ -39,16 +61,20 @@ function signinCheckFunc(signinObj){
 
     signinArr.forEach(function (elem){
         if (signinObj.sMail == elem.mail  && signinObj.sPassword == elem.password){
-            console.log("amit")
             localStorage.setItem("signedinDetails",JSON.stringify(elem))
-            document.getElementById("signinBtn").style.backgroundColor = "#1a73e8";
-            document.getElementById("signinBtn").style.color = "white"
-            window.location.href = "index.html"
-             flag= false;
+            return flag=false;
         }
     })
 
     if (flag == true){
         alert("Wrong Input Details");
+    }
+
+    else{
+        sMail.value = "";
+        sPassword.value = "";
+        setTimeout(() => {
+            window.location = "index.html"
+        },1200)
     }
 }
